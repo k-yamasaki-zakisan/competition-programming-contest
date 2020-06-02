77b0ee67e26ee03426b1ed4d87a05f893fb6fcc5
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-06-03 07:56:37 +0900

    中央値の考察

---
+++ AtCoderBeginnerContest/ABC169E.py
@@ -0,0 +1,18 @@
+#https://atcoder.jp/contests/abc169/tasks/abc169_e
+
+N = int(input())
+A = [0]*N
+B = [0]*N
+for i in range(N):
+  A[i],B[i] = map(int, input().split())
+A.sort()
+B.sort(reverse=True)
+if N%2==1:
+  mid = (N+1)//2-1
+  ans = B[mid]-A[mid]+1
+else:
+  mid1 = N//2-1
+  mid2 = N//2
+  ans = (B[mid1]+B[mid2])-(A[mid1]+A[mid2])+1
+
+print(ans)
