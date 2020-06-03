773efb7b2a2f3ce0a70925593253b7c4efb72d2c
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-06-04 08:04:54 +0900

    グラフ理論

---
+++ AtCoderBeginnerContest/ABC168D.py
@@ -0,0 +1,28 @@
+#https://atcoder.jp/contests/abc168/tasks/abc168_d
+
+from collections import deque
+
+n,m = map(int,input().split())
+
+root = [[] for i in range(n)]
+for _ in range(m):
+    a, b = (int(x) for x in input().split())
+    root[b-1].append(a-1)
+    root[a-1].append(b-1) 
+
+stack=deque([0])
+check = [-1]*n
+check[0] = 0
+
+while len(stack)>0:
+    v = stack.popleft()
+    for i in root[v]:
+        if check[i] == -1:
+            check[i]=v+1
+            stack.append(i)
+if -1 in check:
+    print('No')
+else:
+    print('Yes')
+    for i in range(1,n):
+        print(check[i])
