ff438d56cb56f2ae1c51957dc3f4c6aff8d5c082
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-06-08 14:04:17 +0900

    計算量に気をつけてやるだけ

---
+++ AnotherContest/PAST2G.py
@@ -0,0 +1,35 @@
+#https://atcoder.jp/contests/past202004-open/tasks/past202004_g
+
+from collections import deque
+
+q=int(input())
+
+memo = deque()
+
+for _ in range(q):
+    ans = 0
+    query = input().split()
+    if query[0] == '1':
+      memo.append([query[1] , int(query[2])])
+    else:
+      memo_erase = {}
+      erase_count = int(query[1])
+      while erase_count > 0 and len(memo) > 0:
+        if memo[0][1] <= erase_count:
+          erase_count -= memo[0][1]
+          alf , moji_su = memo.popleft()
+          if alf in memo_erase:
+            memo_erase[alf] += moji_su
+          else:
+            memo_erase[alf] = moji_su
+        else:
+          memo[0][1] -= erase_count
+          if memo[0][0] in memo_erase:
+            memo_erase[memo[0][0]] += erase_count
+          else:
+            memo_erase[memo[0][0]] = erase_count
+          erase_count = 0
+
+      for v in memo_erase.values():
+        ans += v**2
+      print(ans)
