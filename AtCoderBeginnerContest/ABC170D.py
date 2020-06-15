a7ad734ffc5d5c260e0724bf6a4cb4685024c9ad
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-06-15 16:17:57 +0900

    修正_約数問題

--- AtCoderBeginnerContest/ABC170D.py
+++ AtCoderBeginnerContest/ABC170D.py
@@ -1,32 +1,18 @@
 #https://atcoder.jp/contests/abc170/tasks/abc170_d
 
-n=int(input())
-
-a = list(map(int,input().split()))
-
-a.sort()
-
-if n > 1:
-    if a[0] == a[1]:
-        ans = 0
-    else:
-        ans = 1
-else:
+n = int(input())
+a = sorted(list(map(int, input().split())))
+if n == 1:
     print(1)
-    exit()
-
-for i in range(1,n):
-    flag = True
-    for j in range(i):
-        if a[i] % a[j] == 0:
-            flag = False
-            break
-        if int(a[i]**0.5)+1 < a[j]:  
-            break
-    if i == n-1:
-        if flag and a[i] != a[i-1]:
-            ans += 1
-    else:
-        if flag and a[i] != a[i-1] and a[i] != a[i+1]:
-            ans += 1
+    exit()  
+check_list = [0] * (10**6 + 2)
+ans = 0
+for i in range(n):
+    tmp = a[i]
+    while tmp <= a[-1]:
+        check_list[tmp] += 1
+        tmp += a[i]
+for aa in a:
+    if check_list[aa] == 1:
+        ans += 1
 print(ans)
