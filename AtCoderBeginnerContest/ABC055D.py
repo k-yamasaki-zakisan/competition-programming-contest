dc3854475efe89294030248095562cb3d140a220
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-02 09:50:03 +0900

    ４パターン列挙＋１体目の前後で条件が成り立つか確認

--- AtCoderBeginnerContest/ABC055D.py
+++ AtCoderBeginnerContest/ABC055D.py
@@ -4,32 +4,56 @@ n=int(input())
 
 s=list(input())
 
-memo_ss=['S','S']
-memo_sw=['S','W']
-memo_ws=['W','S']
-memo_ww=['W','W']
+memo_ss = ['S','S']
+memo_sw = ['S','W']
+memo_ws = ['W','S']
+memo_ww = ['W','W']
 
-for i in range(1,n):
-    if s[i] == 'o' :
-        if memo_ss[i] == 'S':
-            if memo_ss[i-1] == 'S':
-                memo_ss.append('S')
-            elif memo_ss[i-1] == 'W':
-                memo_ss.append('W')
-        elif memo_ss[i] == 'W':
-            if memo_ss[i-1] == 'S':
-                memo_ss.append('W')
-            elif memo_ss[i-1] == 'W':
-                memo_ss.append('S')
-    elif s[i] == 'x' :
-        if memo_ss[i] == 'S':
-            if memo_ss[i-1] == 'S':
-                memo_ss.append('W')
-            elif memo_ss[i-1] == 'W':
-                memo_ss.append('S')
-        elif memo_ss[i] == 'W':
-            if memo_ss[i-1] == 'S':
-                memo_ss.append('S')
-            elif memo_ss[i-1] == 'W':
-                memo_ss.append('W')
-print(memo_ss)
+def check(memo_ss):
+    for i in range(1,n):
+        if s[i] == 'o' :
+            if memo_ss[i] == 'S':
+                if memo_ss[i-1] == 'S':
+                    memo_ss.append('S')
+                elif memo_ss[i-1] == 'W':
+                    memo_ss.append('W')
+            elif memo_ss[i] == 'W':
+                if memo_ss[i-1] == 'S':
+                    memo_ss.append('W')
+                elif memo_ss[i-1] == 'W':
+                    memo_ss.append('S')
+        elif s[i] == 'x' :
+            if memo_ss[i] == 'S':
+                if memo_ss[i-1] == 'S':
+                    memo_ss.append('W')
+                elif memo_ss[i-1] == 'W':
+                    memo_ss.append('S')
+            elif memo_ss[i] == 'W':
+                if memo_ss[i-1] == 'S':
+                    memo_ss.append('S')
+                elif memo_ss[i-1] == 'W':
+                    memo_ss.append('W')
+    return memo_ss
+
+memo_ss = check(memo_ss)
+memo_sw = check(memo_sw)
+memo_ws = check(memo_ws)
+memo_ww = check(memo_ww)
+
+for memo in [memo_ss,memo_sw,memo_ws,memo_ww]:
+    if memo[0] == memo[-1]:
+        if s[0] == 'o':
+            if memo[0] == 'S' and memo[1] == memo[n-1]:
+                print(''.join(map(str,memo[:n])))
+                exit()
+            elif memo[0] == 'W' and memo[1] != memo[n-1]:
+                print(''.join(map(str,memo[:n])))
+                exit()
+        elif s[0] == 'x':
+                if memo[0] == 'S' and memo[1] != memo[n-1]:
+                    print(''.join(map(str,memo[:n])))
+                    exit()
+                elif memo[0] == 'W' and memo[1] == memo[n-1]:
+                    print(''.join(map(str,memo[:n])))
+                    exit()
+print(-1)
