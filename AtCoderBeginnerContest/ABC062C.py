d8061f171bfef2f092c0043b165efc224d568cf6
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-19 11:50:35 +0900

    場合分け全探査

--- AtCoderBeginnerContest/ABC062C.py
+++ AtCoderBeginnerContest/ABC062C.py
@@ -2,20 +2,23 @@
 
 h,w = map(int,input().split())
 
+ans = float('inf')
+
 if h%3==0 or w%3==0:
     print(0)
     exit()
 
-ans = float('inf')
+#縦割り
+ans = min(ans, w)
+
+#横割り
+ans = min(ans, h)
 
 #縦割り+横半分
 for x in range(1, w+1):
     tate_area = x*h
     yoko_min_area = (w-x)*(h//2)
-    if h%2 == 1:
-        yoko_max_area = (w-x)*(1+h//2)
-    else:
-        yoko_max_area = yoko_min_area
+    yoko_max_area = (w-x)*(h-h//2)
     area_max = max(tate_area, yoko_min_area, yoko_max_area)
     area_min = min(tate_area, yoko_min_area, yoko_max_area)
     ans = min(ans, area_max-area_min)
@@ -25,12 +28,9 @@ for x in range(1, w+1):
 for y in range(h//3, h+1):
     yoko_area = y*w
     tate_min_area = (h-y)*(w//2)
-    if w%2 == 1:
-        tate_max_area = (h-y)*(1+w//2)
-    else:
-        tate_max_area = tate_min_area
+    tate_max_area = (h-y)*(w-w//2)
     area_max = max(yoko_area, tate_min_area, tate_max_area)
     area_min = min(yoko_area, tate_min_area, tate_max_area)
     ans = min(ans, area_max-area_min)
-    #print(ans, yoko_area, tate_min_area)
+
 print(ans)
