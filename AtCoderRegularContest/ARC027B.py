dfcced58feaa4c03347e0fff3ad8d30ccb647cf1
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-08-01 10:29:45 +0900

    リプレイスして数え上げ

--- AtCoderRegularContest/ARC027B.py
+++ AtCoderRegularContest/ARC027B.py
@@ -1,5 +1,40 @@
 #https://atcoder.jp/contests/arc027/tasks/arc027_2
 
+#case_1
+n = int(input())
+s1 = input()
+s2 = input()
+
+for i in range(n):
+    c1 = s1[i]
+    c2 = s2[i]
+    if 'A' <= c1 <= 'Z':
+        if 'A' <= c2 <= 'Z':
+            if c1 != c2:
+                s1 = s1.replace(c2, c1)
+                s2 = s2.replace(c2, c1)
+        else:
+            s1 = s1.replace(c1, c2)
+            s2 = s2.replace(c1, c2)
+    else:
+        if 'A' <= c2 <= 'Z':
+            s1 = s1.replace(c2, c1)
+            s2 = s2.replace(c2, c1)
+
+ans = 1
+d = set()
+for i in range(n):
+    if 'A' <= s1[i] <= 'Z' and s1[i] not in d:
+        d.add(s1[i])
+        if i == 0:
+            ans *= 9
+        else:
+            ans *= 10
+
+print(ans)
+
+
+#case_2
 num_check  = []
 for i in range(10):
     num_check.append(str(i))
