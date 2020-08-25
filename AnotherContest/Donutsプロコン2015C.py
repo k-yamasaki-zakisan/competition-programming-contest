49dac007c79f90e203a095f878666f6a5b5acbb5
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-08-25 17:29:08 +0900

    [fix]リファクタリング

--- AnotherContest/Donutsプロコン2015C.py
+++ AnotherContest/Donutsプロコン2015C.py
@@ -4,18 +4,14 @@ N = int(input())
 
 H = list(map(int,input().split()))
 
-ans = [-1]*N
+memo = []
 
-stack = []
-
-for i,val in enumerate(H):
-    ans[i] = len(stack)
-    while 0 < len(stack):
-        if stack[-1] < val:
-            stack.pop()
+for i in range(N):
+    var =  H[i]
+    print(len(memo))
+    while 0 < len(memo):
+        if memo[-1] < var:
+            memo.pop()
         else:
             break
-    stack.append(val)
-
-for aa in ans:
-    print(aa)
+    memo.append(var)
