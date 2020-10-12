3a333843ac6a7789ae093cb7c7d2a9d2e5f88065
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-10-12 09:42:10 +0900

    [add]最大公約数

---
+++ AtCoderRegularContest/ARC105B.py
@@ -0,0 +1,17 @@
+#https://atcoder.jp/contests/arc105/tasks/arc105_b
+
+import math
+
+N = int(input())
+A = list(map(int,input().split()))
+A = list(set(A))
+A.sort()
+if N == 1:
+    print(A[0])
+    exit()
+
+max_gcd = math.gcd(A[0], A[1])
+for i in range(2,len(A)):
+    max_gcd = math.gcd(max_gcd, A[i])
+
+print(max_gcd)
