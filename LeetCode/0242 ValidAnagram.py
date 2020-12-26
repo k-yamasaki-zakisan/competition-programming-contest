e12a3342ceb472cc96ee9195c95c2dfac5637acf
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-12-26 18:49:09 +0900

    [add]アナグラム

---
+++ LeetCode/0242 ValidAnagram.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/valid-anagram/
+# Runtime: 60 ms, faster than 18.63% of Python3 online submissions for Valid Anagram.
+# Memory Usage: 14.4 MB, less than 58.44% of Python3 online submissions for Valid Anagram.
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        if s == t:
+            return True
+        str_memo = {}
+        for ss in s:
+            if ss in str_memo:
+                str_memo[ss] += 1
+            else:
+                str_memo[ss] = 1
+        for tt in t:
+            if tt in str_memo and str_memo[tt]:
+                str_memo[tt] -= 1
+            else:
+                return False
+        if max(str_memo.values()) == 0:
+            return True
+        else:
+            return False
