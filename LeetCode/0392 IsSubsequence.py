478ed781fb0f733fc1f1cea1e7c23780232c806f
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-01-12 09:41:38 +0900

    [add]ベストプラクティス追加

--- LeetCode/0392 IsSubsequence.py
+++ LeetCode/0392 IsSubsequence.py
@@ -16,3 +16,20 @@ class Solution:
             if is_exit == False:
                 return False
         return True
+
+
+# Runtime: 36 ms, faster than 32.10% of Python3 online submissions for Is Subsequence.
+# Memory Usage: 14.4 MB, less than 23.25% of Python3 online submissions for Is Subsequence.
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+
+        s_index = 0
+        for t_index in range(len(t)):
+            if t[t_index] == s[s_index]:
+                s_index += 1
+            if s_index >= len(s):
+                return True
+        return False
