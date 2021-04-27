9dc08ab82a3203fc1df26750ec5134071b92e41b
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-04-27 17:09:50 +0900

    [add]ソートして貪欲

---
+++ typical90/014 WeUsedToSingASongTogether.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/typical90/tasks/typical90_n
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+A.sort()
+B.sort()
+ans = 0
+for i in range(N):
+    ans += abs(A[i]-B[i])
+print(ans)
