d3a266b3c33cf3f70ab2eb6566c4eb87227793ef
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-05-02 10:22:56 +0900

    [add]ダイクストラ+ワープ処理

--- AnotherContest/ZONe2021E.py
+++ AnotherContest/ZONe2021E.py
@@ -2,9 +2,11 @@
 
 import heapq
 
+# ダイクストラ
+
 
 def dij(cost, start):
-    d = [float('inf')] * (R*C*4)
+    d = [float('inf')] * (R*C*2)
     d[start] = 0
 
     q = [(d[start], start)]
@@ -24,14 +26,13 @@ R, C = map(int, input().split())
 gr, gc = R-1, C-1
 A = [list(map(int, input().split())) for _ in range(R)]
 B = [list(map(int, input().split())) for _ in range(R-1)]
-cost = [[] for _ in range(R*C*3)]
+cost = [[] for _ in range(R*C*2)]
+# 2層コスト作成(1:通常層, 2:ワープ移動層)
 for y in range(R):
     for x in range(C):
         now_pon = y*C+x
-        cost[now_pon].append((2*R*C+C*y+x, 1))
-        cost[2*R*C+C*y+x].append((now_pon, 0))
-        cost[R*C+C*y+x].append((2*R*C+C*y+x, 0))
-        cost[2*R*C+C*y+x].append((R*C+C*y+x, 0))
+        cost[now_pon].append((R*C+C*y+x, 1))
+        cost[R*C+C*y+x].append((now_pon, 0))
         if 0 < y:
             cost[R*C+C*y+x].append((R*C+C*(y-1)+x, 1))
         for my, mx in [[1, 0], [0, 1], [0, -1]]:
@@ -48,4 +49,5 @@ for y in range(R):
                 cost[now_pon].append((next_pon, A[h][w]))
 
 dis = dij(cost, 0)
+# 1層目のゴールの移動コストを出力
 print(dis[R*C-1])
