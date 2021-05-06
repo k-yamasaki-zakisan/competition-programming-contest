ba98445f60467ece4884086273e92c6ca2b1296f
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-05-07 08:21:07 +0900

    [add]謎のコーナーケース

---
+++ typical90/033 NotTooBright.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/typical90/tasks/typical90_ag
+
+H, W = map(int, input().split())
+t = H//2 if H % 2 == 0 else H//2 + 1
+y = W//2 if W % 2 == 0 else W//2 + 1
+ans = t * y
+if H == 1 or W == 1:
+    print(H*W)
+    exit()
+print(ans)
