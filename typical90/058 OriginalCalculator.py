6fe4a43c17e28283b0f55027d352f285e565c38e
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-06-04 23:56:49 +0900

    [add]周期を見つける

--- typical90/058 OriginalCalculator.py
+++ typical90/058 OriginalCalculator.py
@@ -1,5 +1,6 @@
 # https://atcoder.jp/contests/typical90/tasks/typical90_bf
 
+# ボタンを押して過去に同じ数字が出てないか探索
 MOD = 10**5
 N, K = map(int, input().split())
 memo = [N]
@@ -17,8 +18,11 @@ while len(memo) <= K:
         break
     memo.append(next_num)
     memo_list[next_num] = 1 + memo_list[tmp_num]
+
+# ボタン上限回数ならばそのまま出力
 if len(memo) == K+1:
     print(memo[-1])
+# ループする周期を調べて該当のindexを計算して答えを出力
 else:
     K -= (memo_list[first_num]-1)
     targe_cnt = memo_list[first_num]
