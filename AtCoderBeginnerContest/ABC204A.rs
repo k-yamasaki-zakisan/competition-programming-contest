ad74d920cf5f80058ea78e9b36e593f416f74379
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-06-13 00:27:42 +0900

    [add]rust tutorial

---
+++ AtCoderBeginnerContest/ABC204A.rs
@@ -0,0 +1,15 @@
+// https://atcoder.jp/contests/abc204/tasks/abc204_a
+
+use proconio::input;
+
+fn main() {
+    input! {
+        N: usize,
+        A: [usize:N],
+    }
+    for i in 0..N {
+        println!("{}", i)
+    }
+
+    // println!("{}", if x == y { x } else { 3 - x - y });
+}
