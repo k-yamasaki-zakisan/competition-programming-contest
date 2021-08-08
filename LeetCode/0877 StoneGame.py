f1c0bfbab12d0ad8f519a12b77ff570368097cdd
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-08-08 17:04:36 +0900

    [add]ねた

---
+++ LeetCode/0877 StoneGame.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/stone-game/
+# Runtime: 28 ms, faster than 96.54% of Python3 online submissions for Stone Game.
+# Memory Usage: 14.3 MB, less than 50.91% of Python3 online submissions for Stone Game.
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
