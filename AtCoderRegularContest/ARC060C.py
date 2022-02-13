0c42f559a7dcfefd9138a09675a612cca5a2dad1
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-02-13 14:32:03 +0900

    dp

--- AtCoderRegularContest/ARC060C.py
+++ AtCoderRegularContest/ARC060C.py
@@ -1,16 +1,4 @@
-# from collections import Counter
-# import re
-# import copy
-# import itertools
-# from sys import stdin
-# from collections import deque
-# from copy import copy
-# from itertools import combinations
-# from bisect import bisect
-# import heapq
-# import sys
-# from collections import defaultdict
-# memo = defaultdict(int)
+# https://atcoder.jp/contests/arc060/tasks/arc060_a
 
 import sys
 
