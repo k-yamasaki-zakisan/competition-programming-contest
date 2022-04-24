bed21bf508eac2531d4d8f6cb8419d7880659474
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-04-24 17:10:36 +0900

    kotlin 配列の取り扱い

---
+++ AtCoderBeginnerContest/ABC247A.kt
@@ -0,0 +1,9 @@
+fun main() {
+  var S = readLine()!!.split("").filter{ it != "" }
+  var S0 = S[0].toString()
+  var S1 = S[1].toString()
+  var S2 = S[2].toString()
+  var S3 = S[3].toString()
+
+  println("0"+"${S0}"+"${S1}"+"${S2}") 
+}
