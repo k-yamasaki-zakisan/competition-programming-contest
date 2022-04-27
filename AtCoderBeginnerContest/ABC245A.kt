62e51dfc0cc87d248892586a77c6f161d13eead8
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-04-27 21:53:46 +0900

    段々kotlin慣れてきた

---
+++ AtCoderBeginnerContest/ABC245A.kt
@@ -0,0 +1,10 @@
+fun main() {
+  val (A, B, C, D) = readLine()!!.split(" ").map { it.toInt() }
+  if (A < C) {
+    print("Takahashi")
+	} else if(A == C && B <= D) {
+    print("Takahashi")
+	} else {
+    print("Aoki")
+	}
+}
