fe416ec5f2d759a2aabaed53d2ba17420369dfd0
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-07-16 15:15:52 +0900

    ただのdfs

--- LeetCode/0980 UniquePathsIII.py
+++ LeetCode/0980 UniquePathsIII.py
@@ -1,13 +1,15 @@
-from typing import List
+# https://leetcode.com/problems/unique-paths-iii/
+# Runtime: 47 ms, faster than 98.76% of Python3 online submissions for Unique Paths III.
+# Memory Usage: 14 MB, less than 55.75% of Python3 online submissions for Unique Paths III.
 
-# from collections import Counter
-# from collections import defaultdict
-# from bisect import bisect_right
-# from copy import copy
-# from collections import deque
+from typing import List
 
 
 class Solution:
+    def __init__(self):
+        self.ans = 0
+        self.now_cnt = 1
+
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
         max_h = len(grid)
         max_w = len(grid[0])
@@ -18,16 +20,14 @@ class Solution:
                 start_h, start_w = h, grid[h].index(1)
             if 2 in grid[h]:
                 goal_h, goal_w = h, grid[h].index(2)
-        now_cnt = 1
         visited = [[False] * max_w for _ in range(max_h)]
         visited[start_h][start_w] = True
 
-        def dfs(now_h: int, now_w: int, now_cnt: int):
-            global ans
+        def dfs(now_h: int, now_w: int):
             if now_h == goal_h and now_w == goal_w:
-                if now_cnt == wark_cnt:
-                    return 1
-                return 0
+                if self.now_cnt == wark_cnt:
+                    self.ans += 1
+                return
 
             for y, x in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 next_h, next_w = now_h + y, now_w + x
@@ -37,16 +37,11 @@ class Solution:
                     and grid[next_h][next_w] != -1
                     and not visited[next_h][next_w]
                 ):
-                    now_cnt += 1
+                    self.now_cnt += 1
                     visited[next_h][next_w] = True
-                    dfs(next_h, next_w, now_cnt)
-                    now_cnt -= 1
+                    dfs(next_h, next_w)
+                    self.now_cnt -= 1
                     visited[next_h][next_w] = False
 
-        ans = dfs(start_h, start_w, now_cnt)
-        return ans
-
-
-grid = [[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, -1]]
-S = Solution()
-print(S.uniquePathsIII(grid))
+        dfs(start_h, start_w)
+        return self.ans
