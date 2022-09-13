a0604988f813daec9fcceb07ad3af83ef3f1dcf1
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-09-14 00:29:10 +0900

    ただの区間処理

---
+++ LeetCode/1052 Grumpy Bookstore Owner.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/grumpy-bookstore-owner/
+# Runtime: 729 ms, faster than 13.32% of Python3 online submissions for Grumpy Bookstore Owner.
+# Memory Usage: 16.4 MB, less than 48.16% of Python3 online submissions for Grumpy Bookstore Owner.
+
+from typing import List
+
+
+class Solution:
+    def maxSatisfied(
+        self, customers: List[int], grumpy: List[int], minutes: int
+    ) -> int:
+        satisfied = not_satisfied = bonus = 0
+        for i in range(len(customers)):
+            if not grumpy[i]:
+                satisfied += customers[i]
+            else:
+                not_satisfied += customers[i]
+
+            if minutes <= i and grumpy[i - minutes]:
+                not_satisfied -= customers[i - minutes]
+            bonus = max(bonus, not_satisfied)
+            # print(nomal, memo, max_time)
+        return satisfied + bonus
