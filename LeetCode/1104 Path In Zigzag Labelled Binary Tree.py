2f9ba2f9a2d982c440f6f2dec09d4eb0cd1af74a
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-10-01 11:31:34 +0900

    リファクタ

--- LeetCode/1104 Path In Zigzag Labelled Binary Tree.py
+++ LeetCode/1104 Path In Zigzag Labelled Binary Tree.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/path-in-zigzag-labelled-binary-tree/
-# Runtime: 28 ms, faster than 97.32% of Python3 online submissions for Path In Zigzag Labelled Binary Tree.
-# Memory Usage: 13.9 MB, less than 27.52% of Python3 online submissions for Path In Zigzag Labelled Binary Tree.
+# Runtime: 27 ms, faster than 97.65% of Python3 online submissions for Path In Zigzag Labelled Binary Tree.
+# Memory Usage: 13.8 MB, less than 74.16% of Python3 online submissions for Path In Zigzag Labelled Binary Tree.
 
 from typing import List
 
@@ -11,11 +11,10 @@ class Solution:
         # layer内のrangeを事前に算出
         while memo[-1] < label:
             memo.append(memo[-1] * 2)
-        now = label
         # 反転前のルートを算出
-        while now != 1:
-            ans = [now] + ans
-            now //= 2
+        while label != 1:
+            ans = [label] + ans
+            label //= 2
         ans = [1] + ans
         # 反転時のルートに調整
         for i in range(len(ans) - 2, -1, -2):
