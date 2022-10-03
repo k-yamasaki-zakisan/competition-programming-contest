99cb0f2ca438e2be552f5a1da0b83b45a324748e
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-10-04 01:07:29 +0900

    高速化

--- LeetCode/1106 Parsing A Boolean Expression.py
+++ LeetCode/1106 Parsing A Boolean Expression.py
@@ -1,41 +1,43 @@
 # https://leetcode.com/problems/parsing-a-boolean-expression/
-# Runtime: 1358 ms, faster than 5.28% of Python3 online submissions for Parsing A Boolean Expression.
-# Memory Usage: 14.3 MB, less than 12.87% of Python3 online submissions for Parsing A Boolean Expression.
+# Runtime: 454 ms, faster than 5.28% of Python3 online submissions for Parsing A Boolean Expression.
+# Memory Usage: 14.3 MB, less than 19.80% of Python3 online submissions for Parsing A Boolean Expression.
 
 
 class Solution:
     def parseBoolExpr(self, expression: str) -> bool:
         expression = list(expression)
-        while 1 < len(expression):
-            f = -1
-            tmp = []
-            for i in range(len(expression)):
-                if expression[i] == "(":
-                    f = i
-                    tmp = []
-                elif expression[i] == ")":
-                    if expression[f - 1] == "&":
-                        expression = (
-                            expression[: f - 1]
-                            + ["t" if all(tmp) else "f"]
-                            + expression[i + 1 :]
-                        )
-                    elif expression[f - 1] == "|":
-                        expression = (
-                            expression[: f - 1]
-                            + ["t" if any(tmp) else "f"]
-                            + expression[i + 1 :]
-                        )
-                    else:
-                        expression = (
-                            expression[: f - 1]
-                            + ["t" if not tmp[0] else "f"]
-                            + expression[i + 1 :]
-                        )
-                    break
-                else:
-                    if expression[i] == "t":
+        i = 0
+        f = []
+        while 1 < len(expression) and i < len(expression):
+            if expression[i] == "(":
+                f.append(i)
+            elif expression[i] == ")":
+                f_i = f.pop()
+                tmp = []
+                for t in range(f_i, i + 1):
+                    if expression[t] == "t":
                         tmp.append(True)
-                    elif expression[i] == "f":
+                    elif expression[t] == "f":
                         tmp.append(False)
+
+                if expression[f_i - 1] == "&":
+                    expression = (
+                        expression[: f_i - 1]
+                        + ["t" if all(tmp) else "f"]
+                        + expression[i + 1 :]
+                    )
+                elif expression[f_i - 1] == "|":
+                    expression = (
+                        expression[: f_i - 1]
+                        + ["t" if any(tmp) else "f"]
+                        + expression[i + 1 :]
+                    )
+                else:
+                    expression = (
+                        expression[: f_i - 1]
+                        + ["t" if not tmp[0] else "f"]
+                        + expression[i + 1 :]
+                    )
+                i = f_i - 2
+            i += 1
         return True if expression[0] == "t" else False
