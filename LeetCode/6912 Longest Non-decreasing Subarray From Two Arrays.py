c95554574c4cc3a0b00aa4b150f062d26db940bf
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-07-09 13:05:44 +0900

    単調増加部分列

--- LeetCode/6912 Longest Non-decreasing Subarray From Two Arrays.py
+++ LeetCode/6912 Longest Non-decreasing Subarray From Two Arrays.py
@@ -1,20 +1,11 @@
-from typing import List, Optional
-from functools import lru_cache
-
-# from collections import Counter
-
-# from collections import defaultdict
-# from bisect import bisect_right
-# from copy import copy
-# from collections import deque
-
-# MOD = 10**9 + 7
+from typing import List
 
 
 class Solution:
     def maxNonDecreasingLength(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
         dp1, dp2 = [1] * n, [1] * n
+        dp1[0] = dp2[0] = 1
         for i in range(1, n):
             if nums1[i] >= nums1[i - 1]:
                 dp1[i] = dp1[i - 1] + 1
@@ -24,14 +15,4 @@ class Solution:
                 dp2[i] = dp2[i - 1] + 1
             if nums2[i] >= nums1[i - 1]:
                 dp2[i] = max(dp2[i], dp1[i - 1] + 1)
-            print(dp1)
-            print(dp2)
         return max(max(dp1), max(dp2))
-
-
-S = Solution()
-
-nums1 = [8, 7, 4]
-nums2 = [13, 4, 4]
-# 4
-print(S.maxNonDecreasingLength(nums1, nums2))
