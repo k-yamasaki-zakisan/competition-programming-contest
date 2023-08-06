3ec2f0d1ed0455715d6ebae02c93c9ac3c79cf90
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-08-06 13:22:24 +0900

    0-1 dps

--- LeetCode/6951 Find the Safest Path in a Grid.py
+++ LeetCode/6951 Find the Safest Path in a Grid.py
@@ -1,19 +1,10 @@
-from typing import List, Optional
-from functools import lru_cache
-
-# from collections import Counter
-
-# from collections import defaultdict
-# from bisect import bisect_right
-# from copy import copy
-# from collections import deque
-
-# MOD = 10**9 + 7
+from typing import List
 
 
 class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
         from collections import deque
+        from heapq import heappop, heappush
 
         thif_diff = deque()
         H, W = len(grid), len(grid[0])
@@ -39,13 +30,14 @@ class Solution:
                     next_diff = diff + 1
                     diff_memo[next_h][next_w] = next_diff
                     thif_diff.append([next_h, next_w, next_diff])
-        first_move = deque([[0, 0]])
-        sec_move = deque()
+        first_move = [[-diff_memo[0][0], 0, 0]]
+        sec_move = []
         visited = [[-1] * W for _ in range(H)]
         visited[0][0] = diff_memo[0][0]
         while True:
             while len(first_move):
-                now_h, now_w = first_move.popleft()
+                diff, now_h, now_w = heappop(first_move)
+                diff = -diff
                 for next_h, next_w in [
                     [now_h + 1, now_w],
                     [now_h - 1, now_w],
@@ -57,22 +49,25 @@ class Solution:
                         and 0 <= next_w < W
                         and visited[next_h][next_w] == -1
                     ):
-                        if visited[now_h][now_w] <= diff_memo[next_h][next_w]:
-                            visited[next_h][next_w] = visited[now_h][now_w]
-                            first_move.append([next_h, next_w])
+                        if diff <= diff_memo[next_h][next_w]:
+                            visited[next_h][next_w] = diff
+                            heappush(first_move, [-diff, next_h, next_w])
                         else:
-                            sec_move.append([next_h, next_w, now_h, now_w])
+                            heappush(
+                                sec_move,
+                                [
+                                    -diff_memo[next_h][next_w],
+                                    next_h,
+                                    next_w,
+                                    now_h,
+                                    now_w,
+                                ],
+                            )
             while len(sec_move):
-                now_h, now_w, ex_h, ex_w = sec_move.popleft()
-                visited[now_h][now_w] = visited[ex_h][ex_w]
-                first_move.append([now_h, now_w])
-            print(visited)
+                diff, now_h, now_w, ex_h, ex_w = heappop(sec_move)
+                diff = -diff
+                if visited[now_h][now_w] == -1:
+                    visited[now_h][now_w] = diff
+                    heappush(first_move, [-diff, now_h, now_w])
             if visited[-1][-1] != -1:
                 return visited[-1][-1]
-
-
-S = Solution()
-
-grid = [[1, 1, 1], [1, 1, 1], [1, 0, 0]]  # 0
-# grid = [[0, 1, 1], [0, 1, 1], [0, 0, 1]]
-print(S.maximumSafenessFactor(grid))
