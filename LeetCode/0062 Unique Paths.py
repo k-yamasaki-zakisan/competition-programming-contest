a9965655b3711f0a032ca7cad5c813abbe6492cd
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-12-31 14:14:55 +0900

    test

---
+++ LeetCode/0062 Unique Paths.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = [[0] * n for _ in range(m)]
+
+        for h in range(m):
+            for w in range(n):
+                if h == 0 or w == 0:
+                    memo[h][w] = 1
+                    continue
+                memo[h][w] = memo[h - 1][w] + memo[h][w - 1]
+        return memo[-1][-1]
