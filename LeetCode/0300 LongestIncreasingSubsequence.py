23e81bb8d829836d2707c63a04aaeb21d50c0184
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2024-01-06 12:08:46 +0900

    簡単

--- LeetCode/0300 LongestIncreasingSubsequence.py
+++ LeetCode/0300 LongestIncreasingSubsequence.py
@@ -1,10 +1,13 @@
 # https://leetcode.com/problems/longest-increasing-subsequence/
 # Runtime: 60 ms, faster than 95.23% of Python3 online submissions for Longest Increasing Subsequence.
 # Memory Usage: 14.6 MB, less than 63.95% of Python3 online submissions for Longest Increasing Subsequence.
+from typing import List
+
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         from bisect import bisect_left
+
         LIS = [nums[0]]
         for i in range(len(nums)):
             if nums[i] > LIS[-1]:
