e2196d5628231f63cd1e782e575ef9be804264f9
Kazuyoshi Yamasaki <55447682+k-yamasaki-zakisan@users.noreply.github.com>
2024-10-13 10:22:05 +0900

    2分探索

---
+++ 競技プログラミングの鉄則/A13.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val (N, K) = readLine()!!.split(" ").map(String::toInt)
+    val A = readLine()!!.split(" ").map(String::toInt)
+    var ans = 0L
+    for (i in 0..N-1) {
+        val target = K+A[i]
+        var t_i = A.binarySearch(target+1)
+        if (t_i < 0) {
+            t_i = -t_i-2
+        } else {
+            t_i -= 1
+        }
+        if (i < t_i) {
+            ans += t_i-i
+        }
+    }
+    println(ans)
+}
