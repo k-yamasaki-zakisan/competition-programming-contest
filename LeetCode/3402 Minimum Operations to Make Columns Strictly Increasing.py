00c644e94299a086b00f9627530b0644dfcc0964
Kazuyoshi Yamasaki <55447682+k-yamasaki-zakisan@users.noreply.github.com>
2024-12-29 20:24:28 +0900

    簡単

---
+++ LeetCode/3402 Minimum Operations to Make Columns Strictly Increasing.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def minimumOperations(self, grid: List[List[int]]) -> int:
+        ans = 0
+        for j in range(len(grid[0])):
+            for i in range(1, len(grid)):
+                if grid[i][j] <= grid[i - 1][j]:
+                    ans += grid[i - 1][j] + 1 - grid[i][j]
+                    grid[i][j] = grid[i - 1][j] + 1
+        return ans
